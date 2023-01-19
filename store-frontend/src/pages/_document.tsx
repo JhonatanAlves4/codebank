e023ad608900737269291e60316c64d5828919c4
Jhonatan Alves <jhonatan4alves@gmail.com>
2023-01-19 14:17:23 -0300

    feat: setting document

--- store-frontend/src/pages/_document.tsx
+++ store-frontend/src/pages/_document.tsx
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name='theme-color' content='#000000'/>
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+      </Head>
       <body>
         <Main />
         <NextScript />
