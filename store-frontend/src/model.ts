d46a32635ba5512168aae5c05f870fba62d9e526
Jhonatan Alves <jhonatan4alves@gmail.com>
2023-01-20 11:41:57 -0300

    test: adding another fake product

--- store-frontend/src/model.ts
+++ store-frontend/src/model.ts
@@ -14,8 +14,17 @@ export const products: Product[] = [
     name: "produto",
     description: "descrição produto",
     price: 49.99,
-    image_url: "https://source.unsplash.com/random?product",
+    image_url: "https://source.unsplash.com/random?product" + Math.random(),
     slug: "produto-teste",
     created_at: "2023-01-19T00:00:00",
+  },
+  {
+    id: "uuid",
+    name: "Equipamento",
+    description: "Descrição equipamento",
+    price: 149.99,
+    image_url: "https://source.unsplash.com/random?product" + Math.random(), 
+    slug: "equipamento-teste",
+    created_at: "2023-01-20T00:00:00",
   },
 ];
