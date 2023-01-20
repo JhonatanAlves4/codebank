be1addb7b26782fe1e410018659f7ac34542c8cb
Jhonatan Alves <jhonatan4alves@gmail.com>
2023-01-20 14:17:12 -0300

    style: styling page with material-ui

--- store-frontend/src/pages/index.tsx
+++ store-frontend/src/pages/index.tsx
@@ -5,9 +5,11 @@ import {
   CardActions,
   CardContent,
   CardMedia,
+  Grid,
   Typography,
 } from "@material-ui/core";
 import { Product, products } from "@/model";
+import Link from "next/link";
 
 export default function ProductsListPage() {
   return (
@@ -19,29 +21,37 @@ export default function ProductsListPage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-          <Typography
-            component="h1"
-            variant="h3"
-            color="textPrimary"
-            gutterBottom
-          >
-            Produtos
-          </Typography>
-          {products.map((product) => (
-            <Card key={product.id}>
-              <CardMedia image={product.image_url} />
+      <Typography component="h1" variant="h3" color="textPrimary" gutterBottom>
+        Produtos
+      </Typography>
+      <Grid container spacing={4}>
+        {products.map((product) => (
+          <Grid key={product.id} item xs={12} sm={6} md={4}>
+            <Card>
+              <CardMedia
+                style={{ paddingTop: "50%" }}
+                image={product.image_url}
+              />
               <CardContent>
                 <Typography component="h2" variant="h5" gutterBottom>
                   {product.name}
                 </Typography>
               </CardContent>
               <CardActions>
-                <Button size="small" color="primary" component="a">
-                  Detalhes
-                </Button>
+                <Link
+                  href="/products/[slug]"
+                  as={`/products/${product.slug}`}
+                  passHref
+                >
+                  <Button size="small" color="primary" component="a">
+                    Detalhes
+                  </Button>
+                </Link>
               </CardActions>
             </Card>
-          ))}
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 }
