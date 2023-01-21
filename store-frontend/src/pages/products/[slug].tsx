277fe936440529608bf97719fa4ad0d8d5a7fb3f
Jhonatan Alves <jhonatan4alves@gmail.com>
2023-01-20 22:47:18 -0300

    feat: adding 404 page for products that don't exist

--- store-frontend/src/pages/products/[slug].tsx
+++ store-frontend/src/pages/products/[slug].tsx
@@ -11,12 +11,13 @@ import {
 import { Product } from "@/model";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import http from "@/http";
+import axios from "axios";
 
 interface ProductDetailPageProps {
   product: Product;
 }
 
-const ProductDetailPage: NextPage<ProductDetailPageProps> = ({product}) => {
+const ProductDetailPage: NextPage<ProductDetailPageProps> = ({ product }) => {
   return (
     <>
       <Head>
@@ -35,7 +36,7 @@ const ProductDetailPage: NextPage<ProductDetailPageProps> = ({product}) => {
             Comprar
           </Button>
         </CardActions>
-        <CardMedia style={{paddingTop: '50%'}} image={product.image_url} />
+        <CardMedia style={{ paddingTop: "50%" }} image={product.image_url} />
         <CardContent>
           <Typography variant="body2" color="textSecondary" component="p">
             {product.description}
@@ -48,25 +49,35 @@ const ProductDetailPage: NextPage<ProductDetailPageProps> = ({product}) => {
 
 export default ProductDetailPage;
 
-export const getStaticProps: GetStaticProps<ProductDetailPageProps, {slug: string}> = async (context) => {
+export const getStaticProps: GetStaticProps<
+  ProductDetailPageProps,
+  { slug: string }
+> = async (context) => {
   const { slug } = context.params!;
-  const { data: product } = await http.get(`products/${slug}`);
-  
-  return {
-    props: {
-      product,
-      //products: response.data
-    },
-    revalidate: 1 * 60 * 2
-  };
+  try {
+    const { data: product } = await http.get(`products/${slug}`);
+
+    return {
+      props: {
+        product,
+        //products: response.data
+      },
+      revalidate: 1 * 60 * 2,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 404) {
+      return { notFound: true };
+    }
+    throw error;
+  }
 };
 
 export const getStaticPaths: GetStaticPaths = async (context) => {
   const { data: products } = await http.get(`products`);
 
   const paths = products.map((p: Product) => ({
-    params: {slug: p.slug}
-  }))
+    params: { slug: p.slug },
+  }));
 
-  return {paths, fallback: "blocking"}
-}
+  return { paths, fallback: "blocking" };
+};
